9601f039b86b10275226997fd316e106787685c5
petarpetrov <ipetrovbg@gmail.com>
2019-11-01 19:22:22 +0200

    Initial Commit

---
+++ Accounting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accounting
+//
+//  Created by Petar Petrov on 1.11.19.
+//  Copyright © 2019 Petar Petrov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
